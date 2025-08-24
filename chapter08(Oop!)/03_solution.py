de0222f32c281e9941c7bb610b0fff386d719a5c
dubenikhil461 <nikhildubey461@gmail.com>
2025-08-25 02:03:55 +0530

    Add Electric_car class: Introduced a new Electric_car class that inherits from Car, with a constructor to initialize battery size, along with test print statements to demonstrate its functionality.

---
+++ chapter08(Oop!)/03_solution.py
@@ -0,0 +1,13 @@
+class Car:
+    def __init__(self,model,brand): #constructor 
+        self.model = model #self is context (this in js)
+        self.brand = brand
+class Electric_car(Car):
+    def __init__(self,brand,model,battery_size):
+        super().__init__(brand,model)
+        self.battery_size = battery_size
+
+my_car = Electric_car("tata","safari","200mh")
+print(my_car.battery_size)
+print(my_car.brand)
+print(my_car.model)
