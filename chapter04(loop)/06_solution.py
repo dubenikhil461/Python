ee72587a59ec2bcaed764d4598907a3c8ea238df
dubenikhil461 <nikhildubey461@gmail.com>
2025-08-24 15:36:05 +0530

    factorial count

---
+++ chapter04(loop)/06_solution.py
@@ -0,0 +1,10 @@
+# factorial using whileloop
+
+number = 4
+count = number
+factorial =1
+while count>0:
+    factorial = factorial * count
+    count -=1
+
+print(f"factorial of {number} is {factorial}")
