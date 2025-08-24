ac1b28d5b7be0b9964191c4b5180f7ab71466229
dubenikhil461 <nikhildubey461@gmail.com>
2025-08-24 14:35:45 +0530

    Enhance ticket pricing logic in 02_solution.py: Add exit statements after price calculation for both age groups. Create new file 03_solution.py.

--- chapter03(loop)/02_solution.py
+++ chapter03(loop)/02_solution.py
@@ -5,6 +5,8 @@ day = input("Enter day: ").strip().lower()
 if age>=18:
     price = 10 if day=="wednesday" else 12
     print(f'ticket price is {price}')
+    exit()
 elif age<18:
     price = 6 if day=="wednesday" else 8
     print(f'ticket price is {price}')
+    exit()
