7b753712a1881e01e90dfd8c23cb762df2a99582
dubenikhil461 <nikhildubey461@gmail.com>
2025-08-24 16:29:46 +0530

    Enhance list uniqueness checker: Added a second method using a set for improved efficiency in checking for duplicates.

--- chapter04(loop)/10_solution.py
+++ chapter04(loop)/10_solution.py
@@ -1,8 +1,19 @@
 # list uniqueness checker
 
+from enum import unique
+
+
 list = [1,2,3,4,5,2,6]
 for i in list:
     if list.count(i)>1:
         print(f"given list is not unique as {i} repeat more than 1")
         break
 else: print("list is unique")
+
+#method 2
+unique_item = set()
+for item in list:
+    if item in unique_item:
+        print(f"list is not unique")
+        break
+    else: unique_item.add(item)
